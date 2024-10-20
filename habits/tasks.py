3fb4d33dd5639fbd5af2e381800d23783c69f689
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2024-10-20 21:32:20 +0300

    Исправлен баг в check_time_to_trigger_habit с временной зоной. Исправлена строка: datetime_to_trigger_task = habit.datetime_to_trigger_task.astimezone(pytz.timezone(TIME_ZONE))

--- habits/tasks.py
+++ habits/tasks.py
@@ -10,14 +10,12 @@ from config.settings import TG_CHAT_ID, TIME_ZONE
 def check_time_to_trigger_habit() -> None:
     """Функция-задача, которая выполняет отправку, если текущее время подошло к запуску и увеличивает интервал для
     следующего запуска. Должна быть настроена на 1-10 ежеминутные запуски, в идеале"""
-    now = datetime.now().replace(tzinfo=pytz.timezone(TIME_ZONE))
+    now = datetime.now(pytz.timezone(TIME_ZONE))
     habits_list = Habit.objects.all()
     for habit in habits_list:
         if habit.datetime_to_trigger_task:
-            # datetime_to_trigger_task = datetime.strptime(str(habit.datetime_to_trigger_task),'%Y-%m-%d %H:%M:%S.%f%z')
             datetime_to_trigger_task = habit.datetime_to_trigger_task.astimezone(pytz.timezone(TIME_ZONE))
             if habit.datetime_to_trigger_task and now > datetime_to_trigger_task:
-                print(datetime_to_trigger_task)
                 message = f'я буду {habit.activity} в {habit.time} в {habit.place}'
                 send_tg_message(TG_CHAT_ID, message)
                 habit.datetime_to_trigger_task += timedelta(days=int(habit.regularity))  # прибавляем период к следующему запуску
